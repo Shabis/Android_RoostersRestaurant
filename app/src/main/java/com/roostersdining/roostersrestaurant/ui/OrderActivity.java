ae6ece34c44700bd8ad1ceeee98028929a7be618
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-19 16:30:41 -0800

    add logo to mainActivity

--- app/src/main/java/com/roostersdining/roostersrestaurant/ui/OrderActivity.java
+++ app/src/main/java/com/roostersdining/roostersrestaurant/ui/OrderActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
 import com.roostersdining.roostersrestaurant.R;
 
 public class OrderActivity extends AppCompatActivity {
+    private DatabaseReference mOrderReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
