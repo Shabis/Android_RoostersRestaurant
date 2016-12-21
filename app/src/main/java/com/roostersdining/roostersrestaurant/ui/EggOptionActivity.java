60a4afc310e8a6627bc60f412c012ee518d0a86f
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-21 15:11:23 -0800

    add basic logic for clicklisteners

--- app/src/main/java/com/roostersdining/roostersrestaurant/ui/EggOptionActivity.java
+++ app/src/main/java/com/roostersdining/roostersrestaurant/ui/EggOptionActivity.java
@@ -1,15 +1,57 @@
 package com.roostersdining.roostersrestaurant.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.roostersdining.roostersrestaurant.R;
 
-public class EggOptionActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class EggOptionActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.eggsOE) Button mEggsOE;
+    @Bind(R.id.eggsOM) Button mEggsOM;
+    @Bind(R.id.eggsOH) Button mEggsOH;
+    @Bind(R.id.eggsScrambled) Button mEggsScrambled;
+    @Bind(R.id.eggsPoached) Button mEggsPoached;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_egg_option);
+        ButterKnife.bind(this);
+
+        mEggsOE.setOnClickListener(this);
+        mEggsOM.setOnClickListener(this);
+        mEggsOH.setOnClickListener(this);
+        mEggsScrambled.setOnClickListener(this);
+        mEggsPoached.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mEggsOE) {
+            Intent intent = new Intent(EggOptionActivity.this, ToastOptionActivity.class);
+            startActivity(intent);
+        }
+        if (v == mEggsOM) {
+            Intent intent = new Intent(EggOptionActivity.this, ToastOptionActivity.class);
+            startActivity(intent);
+        }
+        if (v == mEggsOH) {
+            Intent intent = new Intent(EggOptionActivity.this, ToastOptionActivity.class);
+            startActivity(intent);
+        }
+        if (v == mEggsScrambled) {
+            Intent intent = new Intent(EggOptionActivity.this, ToastOptionActivity.class);
+            startActivity(intent);
+        }
+        if (v == mEggsPoached) {
+            Intent intent = new Intent(EggOptionActivity.this, ToastOptionActivity.class);
+            startActivity(intent);
+        }
     }
 }
