cdd69de5b25d5776ee9c34957478b22919c1d0f1
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-22 10:52:37 -0800

    implement onClickListener for buttons

--- app/src/main/java/com/roostersdining/roostersrestaurant/ui/MenuActivity.java
+++ app/src/main/java/com/roostersdining/roostersrestaurant/ui/MenuActivity.java
@@ -2,14 +2,35 @@ package com.roostersdining.roostersrestaurant.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.roostersdining.roostersrestaurant.R;
 
-public class MenuActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.newItem) Button mNewItem;
+    @Bind(R.id.finishOrder) Button mFinishOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+
+        mNewItem.setOnClickListener(this);
+        mFinishOrder.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewItem) {
+
+        }
+        if (v == mFinishOrder) {
+
+        }
     }
 }
