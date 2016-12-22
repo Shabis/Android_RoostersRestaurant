c970e00ecf14da0f162c590f16375cb57fc5df83
Shelby Clayton <shelby_clayton@hotmail.com>
2016-12-22 12:43:02 -0800

    refactor orderActivity into two activities

--- app/src/main/java/com/roostersdining/roostersrestaurant/ui/OrderActivity.java
+++ app/src/main/java/com/roostersdining/roostersrestaurant/ui/OrderActivity.java
@@ -3,24 +3,13 @@ package com.roostersdining.roostersrestaurant.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.roostersdining.roostersrestaurant.Constants;
 import com.roostersdining.roostersrestaurant.R;
-import com.roostersdining.roostersrestaurant.models.BreakfastItem;
-import com.roostersdining.roostersrestaurant.models.Order;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
